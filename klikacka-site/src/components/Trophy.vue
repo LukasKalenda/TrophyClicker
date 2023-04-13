d5d4dcd90589dbb44b8fc53e728a32a175ec0595
LukasKalenda <kalenda.lu.2020@skola.ssps.cz>
2023-04-13 13:11:16 +0200

    Pridan Trophy.vue

---
+++ klikacka-site/src/components/Trophy.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <h2>Továrna na pohárky</h2>
+    <aside>
+      <p>87000</p>
+      <p>tisíc pohárků</p>
+      <p>za sekundu: 4,256</p>
+    </aside>
+
+    <section>
+      <img src="../assets/icons/trophy-main.png" alt="Main Trophy/Cup" />
+    </section>
+  </main>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
